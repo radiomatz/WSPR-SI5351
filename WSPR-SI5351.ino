74a01f2367747db356115e6fe9fff18737bf1ac8
Matthias DM2HR <dm2hr@hmro.de>
2026-01-24 16:49:35 +0100

    backspace handling, better serial readability

--- WSPR-SI5351.ino
+++ WSPR-SI5351.ino
@@ -19,7 +19,10 @@ unsigned long freq;
 #define WSPR_TONE_SPACING 146  // ~1.46 Hz
 #define WSPR_DELAY 683         // Delay value for WSPR
 #define WSPR_DEFAULT_FREQ_40m  7038600UL // 40m
+#define WSPR_DEFAULT_FREQ_30m 10138700UL // 30m
 #define WSPR_DEFAULT_FREQ_20m 14095600UL // 20m
+#define WSPR_DEFAULT_FREQ_17m 18104600UL // 17m
+#define WSPR_DEFAULT_FREQ_15m 21094600UL // 15m
 
 // #####################################
 // below are all user configuratable items
@@ -27,9 +30,9 @@ unsigned long freq;
 
 // Calibration: see "Etherkit SI5351" Library: si5351_calibration example
 // calib: lower_cal=higher_freq, 1.46 Hz =~ 100 cal
+// SI5351 is very unstable in his frequency when temperature changes on its surface!
 
-// #define CALIBRATION 147400L // warm (25°C)
-#define CALIBRATION 148600L // cold (19°C)
+#define CALIBRATION 147300L // at the moment, if room is warm or even not :-)
 
 unsigned long mainQRG = WSPR_DEFAULT_FREQ_20m;
 char call[13] = "DM2HR"; // size: max 12 + NULL
@@ -101,8 +104,11 @@ void setup() {
   // Initialize the Si5351
   // Change the 2nd parameter in init if using a ref osc other
   // than 25 MHz
-  if (!si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0))
-    Serial.println("Error init!");
+  while ( !si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0) ) {
+      Serial.println("Error init SI5351, check Cables!");
+      delay(2500);
+  }
+  
   si5351.set_correction(CALIBRATION, SI5351_PLL_INPUT_XO);
   si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLA);
   si5351.pll_reset(SI5351_PLLB);
@@ -137,12 +143,14 @@ void loop() {
           if ( Serial.available() ) {
             c = Serial.read();
             Serial.print(c);
-            sein += c;
+            if ( c == 8 && sein.length() > 0 ) // ^H, Backspace
+              sein.remove(sein.length() - 1);
+            else
+              sein += c;
           }
         } while ( c != 0x0d && c != 0x0a);
         while (Serial.available()) {  // NL, CR and any accidentally typed things
           c = Serial.read();
-          Serial.print(c);
         }
         if ( sein != "" ) {
             qrgin = sein.toInt();
@@ -152,14 +160,18 @@ void loop() {
           wsprQRG = qrgin;
         }
       }
+
       freq = mainQRG + wsprQRG;
       now = millis() / 1000;
+
       if ( wsprQRG > 0 ) {
-        Serial.println("\r ... sending now(" + 
-            String(now) + ") on " + 
-            String(mainQRG) + " + " + 
-            String(wsprQRG) + " = " + 
-            String(freq) );
+        
+        Serial.println();
+        Serial.print( " ... sending now(" + String(now) );
+        Serial.print( ") on " + String(mainQRG) );
+        Serial.print( " + " + String(wsprQRG) );
+        Serial.println( " = " + String(freq) );
+
         encode();
       }
       sein = "";
